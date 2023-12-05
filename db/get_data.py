d5d2f938ae1ee71bfe7f593b1ff629607c3ecb1f
Mark <prodius.mark@mail.ru>
2023-12-05 16:46:36 +0300

    change sql request

--- db/get_data.py
+++ db/get_data.py
@@ -4,8 +4,15 @@ cursor = connection.cursor()
 
 
 def get_expressions(get_all=False):
-
-    select_query = 'SELECT expression FROM expressions'
+    select_query = """
+        select
+            case
+                when definiton is not null and example is not null then "expression" || ' - ' || definiton || ' - ' || example
+                when definiton is not null and example is null then "expression" || ' - ' || definiton
+                when definiton is null and example is not null then "expression" || ' - ' || example
+                when definiton is null and example is null then "expression"
+            end as ex_def   from expressions e 
+    """
 
     if not get_all:
         select_query += ' WHERE is_hidden = False'
